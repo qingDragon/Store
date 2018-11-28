de9ef9f81dfb12e216a0fb377c9035bc29400668
West <921971961@qq.com>
2018-11-29 02:02:45 +0800

    4

--- app/src/main/java/com/big/yanzhuang/store/FirstActivity.java
+++ app/src/main/java/com/big/yanzhuang/store/FirstActivity.java
@@ -6,10 +6,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.gson.JsonObject;
 import com.jauker.widget.BadgeView;
 
-public class FirstActivity extends AppCompatActivity implements View.OnClickListener{
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.util.HashMap;
+import java.util.Map;
 
+public class FirstActivity extends AppCompatActivity implements View.OnClickListener{
+    private String data;//http请求返回的数据
+    private String order;//处理后的订单字符串
+    public static OrderList orderList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,11 +29,42 @@ public class FirstActivity extends AppCompatActivity implements View.OnClickList
         //添加角标通知
         BadgeView badgeView = new BadgeView(getBaseContext());
         badgeView.setTargetView(bt1);
-        badgeView.setBadgeCount(3);
-        badgeView.setTextSize(20);
+        badgeView.setBadgeCount(1);
+        badgeView.setTextSize(25);
 
 
         bt1.setOnClickListener(this);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Map<String,String> body = new HashMap<>();
+                    Map<String,String> body2 = new HashMap<>();
+                    body.put("usernum","662995");
+                    body.put("userpwd","123456");
+
+                    System.out.println("lalla");
+                    String s = HttpSend.doPost("https://www.kpcodingoffice.com/index",body);
+                    System.out.println(s);
+
+                    System.out.println(Tools.str2Json(s).get("token").getAsString());
+                    body2.put("usernum","662995");
+                    body2.put("token",Tools.str2Json(s).get("token").getAsString());
+                    //第二次请求获得的String
+                    data= HttpSend.doPost("https://www.kpcodingoffice.com/orderdata",body2);
+                    System.out.println(data);
+                    System.out.println(str2order(data).getUsers());
+                } catch (NoSuchProviderException e) {
+                    e.printStackTrace();
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                } catch (KeyManagementException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
 
     }
 
@@ -35,4 +76,17 @@ public class FirstActivity extends AppCompatActivity implements View.OnClickList
                 startActivity(intent);
         }
     }
+    public OrderList str2order(String string){
+        JsonObject jsonObject = Tools.str2Json(string);
+        String str = jsonObject.get("datas").toString();
+        order = str.substring(1,str.length()-1);
+        jsonObject = new JsonObject();
+        jsonObject = Tools.str2Json(order);
+        String users = jsonObject.get("users").getAsString();
+        String signuptime = jsonObject.get("signuptime").getAsString();
+        String goodscomments = jsonObject.get("goodscomments").getAsString();
+        orderList = new OrderList(users,signuptime,goodscomments);
+        return orderList;
+    }
+
 }
--- app/src/main/java/com/big/yanzhuang/store/SecondActivity.java
+++ app/src/main/java/com/big/yanzhuang/store/SecondActivity.java
@@ -1,34 +1,13 @@
 package com.big.yanzhuang.store;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.os.Build;
-import android.os.StrictMode;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import com.google.gson.JsonObject;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
-
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
@@ -37,7 +16,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.big.yanzhuang.store.HttpSend.doPost;
 import static com.big.yanzhuang.store.R.layout.activity_second;
 
 public class SecondActivity extends AppCompatActivity {
@@ -45,7 +23,7 @@ public class SecondActivity extends AppCompatActivity {
     private ListView listView;
     private List<OrderList> data_list;
     private MyAdapter myAdapter;
-    private String data;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,62 +35,30 @@ public class SecondActivity extends AppCompatActivity {
 //        LinearLayout layout = (LinearLayout) findViewById(R.id.layout_01);
 //        layout.addView(view);
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Map<String,String> body = new HashMap<>();
-                    Map<String,String> body2 = new HashMap<>();
-                    body.put("usernum","662995");
-                    body.put("userpwd","123456");
 
-                    System.out.println("lalla");
-                    String s = HttpSend.doPost("https://www.kpcodingoffice.com/index",body);
-                    System.out.println(s);
-
-                    System.out.println(Tools.str2Json(s).get("token").getAsString());
-                    body2.put("usernum","662995");
-                    body2.put("token",Tools.str2Json(s).get("token").getAsString());
-                    //第二次请求获得的String
-                    data= HttpSend.doPost("https://www.kpcodingoffice.com/orderdata",body2);
-
-                } catch (NoSuchProviderException e) {
-                    e.printStackTrace();
-                } catch (NoSuchAlgorithmException e) {
-                    e.printStackTrace();
-                } catch (KeyManagementException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
         initView();
-        initData(data);
+        initData(FirstActivity.orderList);
+        System.out.println("hello");
+//        System.out.println(str2order(data).getUsers());
+//        Log.d("输出",str2order(data).toString());
 
 
     }
 
 
     public void initView(){
+
         listView = (ListView) findViewById(R.id.list_view);
     }
-    public void initData(String str){
-        String str1,str2,str3;
-        JsonObject jsonObject = new JsonObject();
-        jsonObject = (JsonObject)Tools.str2Json(str).get("datas");
-        str1 = jsonObject.get("users").getAsString();
-        str2 = jsonObject.get("signuptime").getAsString();
-        str3 = jsonObject.get("goodscomments").getAsString();
-        System.out.println(str1);
+
+
+    public void initData(OrderList orderList){
         data_list = new ArrayList<OrderList>();
         //后面根据订单数量加循环
-        OrderList orderList = new OrderList(str1,str2,str3);
         data_list.add(orderList);
         myAdapter = new MyAdapter(this,data_list);
         listView.setAdapter(myAdapter);
     }
-
 
 
 }
